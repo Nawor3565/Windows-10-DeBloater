7a171e0fea9c8633c2499604df439673537b6552
andymello <andy.mello@gmail.com>
2017-10-09 21:13:58 -0400

    Update RUN-AS-ADMIN.bat
    Add "–ExecutionPolicy Bypass" to the powershell commands

--- Win10Debloater/RUN-AS-ADMIN.bat
+++ Win10Debloater/RUN-AS-ADMIN.bat
@@ -1,7 +1,5 @@
 pushd %~dp0
 registrychanges.reg
-AllowPowerShellScripts.reg
-PowerShell ".\RemoveBuiltInPrograms.ps1"
-PowerShell ".\DisableTelemetryServices.ps1"
-DisallowPowerShellScripts.reg
+PowerShell –ExecutionPolicy Bypass ".\RemoveBuiltInPrograms.ps1"
+PowerShell –ExecutionPolicy Bypass ".\DisableTelemetryServices.ps1"
 FinishedMessage.vbs
